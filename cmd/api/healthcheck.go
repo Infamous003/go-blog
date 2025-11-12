9a534397e4a9f3dfbf64a6fb5d4d2b5d74c29112
Infamous003 <arfathsyed0312@gmail.com>
2025-11-12 15:50:37 +0530

    feat: add helper to write JSON and use it in healthcheck handler

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := map[string]any{
-		"status":      "available",
-		"environment": app.cfg.env,
-		"version":     version,
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.cfg.env,
+			"version":     version,
+		},
 	}
 
-	err := json.NewEncoder(w).Encode(js)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		http.Error(w, "something went wrong with the server", http.StatusInternalServerError)
+		return
 	}
 }
